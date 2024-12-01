1518e2653ad1bb2b3f094cc558291d2be7ba39d9
Simon Benfold <simon@benfold.com>
2024-12-01 16:40:35 +0000

    Solve 1b

--- 1/solution.el
+++ 1/solution.el
@@ -1,4 +1,9 @@
-(->> (f-read-text "example-input.txt")
+(require 'dash)
+(require 'f)
+(require 's)
+
+;; 1a
+(->> (f-read-text "input.txt")
    (s-trim)
    (s-lines)
    (-map #'s-split-words)
@@ -9,3 +14,15 @@
    (-map #'abs)
    (-sum)
    )
+
+;; 1b
+(-let* (((a b) (->> (f-read-text "input.txt")
+                  (s-trim)
+                  (s-lines)
+                  (-map #'s-split-words)
+                  (-map (-partial #'-map #'string-to-number))
+                  (-unzip)))
+        (freqs (-frequencies b)))
+  (->> a
+     (--map (* it (alist-get it freqs 0)))
+     (-sum)))
