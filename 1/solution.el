50fe6c27dbacc5ef7bb3d341607b2a5be33af039
Simon Benfold <simon@benfold.com>
2024-12-02 17:10:36 +0000

    Minor cleanup to 1

--- 1/solution.el
+++ 1/solution.el
@@ -3,25 +3,22 @@
 (require 's)
 
 ;; 1a
-(->> (f-read-text "input.txt")
-   (s-trim)
-   (s-lines)
-   (-map #'s-split-words)
-   (-map (-partial #'-map #'string-to-number))
-   (-unzip)
+(defun aoc/1/read-input ()
+  (->> (f-read-text "input.txt")
+     (s-trim)
+     (s-lines)
+     (-map #'s-split-words)
+     (-map (-partial #'-map #'string-to-number))
+     (-unzip)))
+
+(->> (aoc/1/read-input)
    (--map (-sort #'< it))
    (apply #'-zip-with #'-)
    (-map #'abs)
-   (-sum)
-   )
+   (-sum))
 
 ;; 1b
-(-let* (((a b) (->> (f-read-text "input.txt")
-                  (s-trim)
-                  (s-lines)
-                  (-map #'s-split-words)
-                  (-map (-partial #'-map #'string-to-number))
-                  (-unzip)))
+(-let* (((a b) (aoc/1/read-input))
         (freqs (-frequencies b)))
   (->> a
      (--map (* it (alist-get it freqs 0)))
