d6606afc9d20357633552834f6385ded0e134cb7
Simon Benfold <simon@benfold.com>
2024-12-02 16:58:13 +0000

    Clean up 2

--- 2/solution.el
+++ 2/solution.el
@@ -2,67 +2,31 @@
 (require 'f)
 (require 's)
 
-;; 2a => 341
+;; 2a
 (defun aoc/2/safe? (a)
   (let ((diffs (-zip-with #'- (cdr a) a)))
     (and
      (apply #'= (-map #'cl-signum diffs))
-     (-every (-partial #'>= 3) (-map #'abs diffs))
-     (not (= (car diffs) 0)))))
+     (--every (<= (abs it) 3) diffs)
+     (not (zerop (car diffs))))))
 
-(->> (f-read-text "input.txt")
-   (s-trim)
-   (s-lines)
-   (-map #'s-split-words)
-   (-map (-partial #'-map #'string-to-number))
-   (-map #'aoc/2/safe?)
-   (-non-nil)
-   (length)
-   )
+(defun aoc/2/solve (safe-func)
+  (->> (f-read-text "input.txt")
+     (s-trim)
+     (s-lines)
+     (-map #'s-split-words)
+     (-map (-partial #'-map #'string-to-number))
+     (-map safe-func)
+     (-non-nil)
+     (length)
+     )
+  )
+(aoc/2/solve #'aoc/2/safe?)
 
-;; 2b =? !405
-;; Not 64 either (405 - 341)
+;; 2b
 (defun aoc/2/dampened-safe? (a)
   (->> (--map-indexed (-remove-at it-index a) a)
      (-map #'aoc/2/safe?)
      (-non-nil)
-     ;; (-first #'identity)
      ))
-
-(->> (f-read-text "input.txt")
-   (s-trim)
-   (s-lines)
-   (-map #'s-split-words)
-   (-map (-partial #'-map #'string-to-number))
-   ;; (--filter (not (aoc/2/safe? it)))
-   (-map #'aoc/2/dampened-safe?)
-   ;; (-annotate #'aoc/2/dampened-safe?)
-   (-non-nil)
-   (length)
-   )
-
-(apply #'some #'identity (list nil t t))
-
-;; TODO Call safe for each element removed
-(aoc/2/safe? (list 1 3 2 4 5))
-
-(let ((a (list 1 3 2 4 5)))
-  (->> (--map-indexed (-remove-at it-index a) a)
-     (-map #'aoc/2/safe?)
-     ;; (-non-nil)
-     (-first #'identity)
-     )
-  )
-
-(aoc/2/dampened-safe? (list 7 6 4 2 1))
-(aoc/2/dampened-safe? (list 1 2 7 8 9))
-(aoc/2/dampened-safe? (list 9 7 6 2 1))
-(aoc/2/dampened-safe? (list 1 3 2 4 5))
-(aoc/2/dampened-safe? (list 8 6 4 4 1))
-(aoc/2/dampened-safe? (list 1 3 6 7 9))
-
-
-
-
-
-((t) (t t) (t) (t) nil nil nil (t) nil (t t) nil nil nil nil nil nil nil nil nil nil (t) nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) (t t) (t) (t) nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil (t) nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) (t t) (t) (t) nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t t) (t t) (t) (t) (t t) nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil (t) nil nil nil (t) nil nil nil nil nil (t) nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil (t) nil (t t) nil nil nil nil nil nil nil nil nil (t) nil nil nil (t t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil (t) (t t) nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil (t) nil nil nil (t t) nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t t) nil nil nil nil nil nil nil nil (t) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil (t t) (t t t t) (t t t t t t t) (t t t t) (t t t) (t t t t) (t t) (t t t t) (t t) (t t t) (t t t t t t) (t t t t t t) (t t) (t t t) (t t t) (t t t) (t t t) (t t) (t t) (t t t t) (t t t t) (t t) (t t t t) (t t) (t t t t) (t t) (t t t) (t t) (t t) (t t t t) (t t) (t t t) (t t t t) (t t t) (t t t t) (t t) (t t t) (t t t t t) (t t) (t t) (t t) (t t) (t t t) (t t t t t) (t t t t t) (t t) (t t t t t) (t t t) (t t t t t) (t t t t t) (t t t t t t t) (t t t) (t t t t t t) (t t t t t t) (t t t) (t t t t t t t) (t t t t) (t t) (t t t) (t t t t) (t t t) (t t t t) (t t t t) (t t t t t) (t t t) (t t t t t) (t t) (t t t) (t t) (t t t t) (t t t t t t) (t t) (t t) (t t t t) (t t t t t t) (t t t t) (t t t) (t t) (t t t t) (t t t t) (t t t) (t t) (t t t t t) (t t) (t t t t) (t t t t t) (t t t) (t t) (t t) (t t t t) (t t) (t t t t t) (t t) (t t t t t t) (t t t t t) (t t) (t t t) (t t t) (t t) (t t) (t t t t) (t t t t t) (t t t) (t t t t t t) (t t t) (t t t t t) (t t t) (t t t t) (t t t) (t t) (t t) (t t t t) (t t t) (t t) (t t) (t t t) (t t) (t t t t) (t t t t t) (t t t t) (t t t t) (t t t) (t t t t t) (t t) (t t t t t) (t t t t t t) (t t) (t t) (t t) (t t) (t t) (t t) (t t t t) (t t t t) (t t t) (t t t t t) (t t) (t t t) (t t t t) (t t) (t t t t t) (t t) (t t t) (t t t t) (t t t) (t t t) (t t t t t t) (t t) (t t t) (t t t t) (t t t) (t t) (t t) (t t t t t t t) (t t t) (t t t t) (t t) (t t t) (t t t) (t t t) (t t t t) (t t t t t t) (t t t t) (t t) (t t t) (t t) (t t t t t t) (t t) (t t) (t t t) (t t t t t t t) (t t t t t) (t t t) (t t t t) (t t t t) (t t t) (t t t t t) (t t t t t t) (t t t t) (t t t) (t t t t) (t t) (t t t) (t t t t) (t t t) (t t t t t) (t t t) (t t t t) (t t t) (t t t) (t t t t t) (t t t) (t t t) (t t) (t t t t) (t t t t t t t t) (t t) (t t t t t) (t t t t t) (t t) (t t) (t t t t t t t) (t t) (t t t t t) (t t) (t t t) (t t t t) (t t t t) (t t t t t) (t t) (t t t) (t t t t) (t t) (t t) (t t t) (t t t t) (t t) (t t t t t) (t t t) (t t t t t) (t t t t) (t t t t t) (t t t t t t t) (t t) (t t t) (t t t) (t t t) (t t t) (t t t t t) (t t) (t t t t) (t t t t) (t t t t t) (t t t t) (t t) (t t t t t t) (t t t) (t t t t) (t t t t) (t t t) (t t t) (t t t) (t t t t t t t) (t t t t) (t t t t) (t t t) (t t) (t t t t) (t t) (t t t t) (t t t) (t t t t) (t t t t t t) (t t t t) (t t t t t) (t t) (t t t) (t t t) (t t t t) (t t t) (t t t t t t t t) (t t t t t) (t t t t t) (t t) (t t t t t) (t t t t t) (t t) (t t t t) (t t) (t t t t) (t t t t) (t t) (t t t) (t t t) (t t t) (t t t t) (t t t) (t t t) (t t t) (t t t) (t t t t t) (t t t) (t t t t) (t t t t) (t t t) (t t t t t) (t t) (t t) (t t) (t t t t) (t t) (t t t) (t t) (t t t) (t t t t) (t t t) (t t t t) (t t t t) (t t t t t) (t t) (t t t t) (t t t t) (t t t t) (t t t t) (t t t t) (t t t t) (t t t t t) (t t t) (t t t t t t) (t t t) (t t t t t) (t t t) (t t t) (t t t) (t t t) (t t) (t t t t) (t t t t) (t t t) (t t t) (t t t t) (t t) (t t t) (t t t t) (t t t t) (t t t t) (t t t) (t t) (t t) (t t) (t t t) (t t t t t t) (t t t t t) (t t t) (t t t) (t t t t t) (t t t) (t t t t t) (t t t) (t t) (t t))
+(aoc/2/solve #'aoc/2/dampened-safe?)
