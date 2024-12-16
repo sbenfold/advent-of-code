488e24a0617032b4b75bda8db737eb9b0a56404c
Simon Benfold <simon@benfold.com>
2024-12-16 13:52:54 +0000

    WIP 5b. Overflows stack, maybe algorithm doesn't always terminate.

--- 2024/5/solution.el
+++ 2024/5/solution.el
@@ -1,10 +1,15 @@
 ;;; solution.el --- Advent of Code 2024 Day 5 solution  -*- lexical-binding: t -*-
 
+;; Default
+(setq max-lisp-eval-depth 1600)
+;; 5x default
+;; (setq max-lisp-eval-depth 8000)
+
 (require 'generator)
 (require 'ht)
 
-(defconst aoc/5/input-file "input.txt")
-;; (defconst aoc/5/input-file "example-input.txt")
+;; (defconst aoc/5/input-file "input.txt")
+(defconst aoc/5/input-file "example-input.txt")
 
 (defun aoc/5/validate-update (pages deps-ht)
   (-all? #'null
@@ -13,7 +18,7 @@
            (ht-get deps-ht (nth it pages))
            (-take it pages)
            )
-          (-iota (1+ (length pages)) 1)
+          (-iota (length pages) 1)
           )))
 
 ;; 5a
@@ -40,6 +45,76 @@
       ))
   (->> updates
      (--filter (aoc/5/validate-update it deps-ht))
+     (--map (nth (/ (length it) 2) it))
+     (-sum)
+     )
+  )
+
+(defun aoc/5b/fix-update (pages deps-ht)
+  ;; TODO: Take the first error and recurse with the missing dependency moved in front
+  ;;   So if '(75 97 47 61 53) fails on 97 because 97 should've come first
+  ;;   Recurse with '(97 75 47 61 53)
+  ;; TODO We want the index of the first failure (add 1 to index)
+  (-if-let
+      (error-index
+       (--find-index
+        (-intersection
+         (ht-get deps-ht (nth it pages))
+         (-take it pages)
+         )
+        (-iota (length pages) 1)
+        ))
+      (let ((intersection (-intersection
+                           (ht-get deps-ht (nth (1+ error-index) pages))
+                           (-take (1+ error-index) pages)
+                           )))
+        ;; TODO Find index of (car intersection) and insert it at error-index
+        (let* ((old-index (-elem-index (car intersection) pages))
+               (new-pages (-remove-at old-index
+                                      (-insert-at (+ error-index 2) (nth old-index pages) pages)
+                                      ))
+               )
+          ;; (message "Recurse %s -> %s" pages new-pages)
+          (aoc/5b/fix-update new-pages deps-ht)
+          )
+
+        )
+    pages
+    )
+  )
+
+;; 5b
+(-let* ((lines (s-lines (f-read-text aoc/5/input-file)))
+        ((deps-raw . updates-raw) (-split-on "" lines))
+        (deps (->> deps-raw
+                 (-sort #'s-less?)
+                 (--map (s-split "|" it))
+                 (-map (-partial #'-map #'string-to-number))
+                 (-partition-by #'car)
+                 ))
+        (updates (->> updates-raw
+                    (car)
+                    (--map (s-split "," it))
+                    (-map (-partial #'-map #'string-to-number))
+                    ))
+        (deps-ht (ht-create))
+        )
+  (->> deps
+     (--map
+      (let ((unzipped (-unzip-lists it)))
+        ;; (message "Add: %s -> %s" (caar unzipped) (cadr unzipped))
+        (ht-set! deps-ht (caar unzipped) (cadr unzipped))
+        )
+      ))
+
+  ;; (let ((pages (list 75 97 47 61 53)))
+  ;;   (aoc/5b/fix-update pages deps-ht)
+  ;;   ;; (ht-get deps-ht (nth 1 pages))
+  ;;   ;; (-take 1 pages)
+  ;;   )
+  (->> updates
+     (--remove (aoc/5/validate-update it deps-ht))
+     (--map (aoc/5b/fix-update it deps-ht))
      (--map (nth (/ (length it) 2) it))
      (-sum)
      )
