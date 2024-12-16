867585eace00d3823806613d38872da0398b38d1
Simon Benfold <simon@benfold.com>
2024-12-16 12:27:51 +0000

    Solve 5a

--- 2024/5/solution.el
+++ 2024/5/solution.el
@@ -3,38 +3,44 @@
 (require 'generator)
 (require 'ht)
 
-;; (defconst aoc/5/input-file "input.txt")
-(defconst aoc/5/input-file "example-input.txt")
+(defconst aoc/5/input-file "input.txt")
+;; (defconst aoc/5/input-file "example-input.txt")
 
-(defun aoc/5/read-input ()
-  (-let* ((lines (s-lines (f-read-text aoc/5/input-file)))
-          ((deps-raw . pages-raw) (-split-on "" lines))
-          (deps (->> deps-raw
-                   (-sort #'s-less?)
-                   (--map (s-split "|" it))
-                   (-map (-partial #'-map #'string-to-number))
-                   (-partition-by #'car)
-                   ))
-          (pages (->> pages-raw
+(defun aoc/5/validate-update (pages deps-ht)
+  (-all? #'null
+         (--map
+          (-intersection
+           (ht-get deps-ht (nth it pages))
+           (-take it pages)
+           )
+          (-iota (1+ (length pages)) 1)
+          )))
+
+;; 5a
+(-let* ((lines (s-lines (f-read-text aoc/5/input-file)))
+        ((deps-raw . updates-raw) (-split-on "" lines))
+        (deps (->> deps-raw
+                 (-sort #'s-less?)
+                 (--map (s-split "|" it))
+                 (-map (-partial #'-map #'string-to-number))
+                 (-partition-by #'car)
+                 ))
+        (updates (->> updates-raw
                     (car)
                     (--map (s-split "," it))
                     (-map (-partial #'-map #'string-to-number))
                     ))
-          (deps-ht (ht-create))
-          )
-    (->> deps
-       (--map
-        (let ((unzipped (-unzip it)))
-          (ht-set! deps-ht (caar unzipped) (cadr unzipped))
-          )
-        ))
-    ;; (ht->plist deps-ht)
-    ;; updates
-    ))
-
-;; 5a
-;; TODO Build a DAG from each dep pair. DAG is page -> dependencies
-;; TODO Filter out incorrectly ordered updates
-;;   TODO Iterate through pages and look up deps. Build a set of available deps while iterating.
-;; TODO Get middle page number
-;; TODO Sum
+        (deps-ht (ht-create))
+        )
+  (->> deps
+     (--map
+      (let ((unzipped (-unzip-lists it)))
+        (ht-set! deps-ht (caar unzipped) (cadr unzipped))
+        )
+      ))
+  (->> updates
+     (--filter (aoc/5/validate-update it deps-ht))
+     (--map (nth (/ (length it) 2) it))
+     (-sum)
+     )
+  )
