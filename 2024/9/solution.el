661b9faf5fb7d48c8c0d29a4665845fe2a73803c
Simon Benfold <simon@benfold.com>
2024-12-23 00:56:02 +0000

    Solve 9a

--- 2024/9/solution.el
+++ 2024/9/solution.el
@@ -4,9 +4,9 @@
 (defconst aoc/9/input-file "example-input.txt")
 
 (defun aoc/9/expand-disk-pair (index pair)
-  (concat
-   (s-repeat (car pair) (number-to-string index))
-   (s-repeat (cadr pair) ".")))
+  (append
+   (-repeat (car pair) index)
+   (-repeat (cadr pair) nil)))
 
 (defun aoc/9/to-block-map (disk-map)
   (->> (concat disk-map "0")
@@ -14,34 +14,37 @@
      (-remove #'s-blank?)
      (-map #'string-to-number)
      (-partition 2)
-     (-map-indexed #'aoc/9/expand-disk-pair)))
+     (-map-indexed #'aoc/9/expand-disk-pair)
+     (-flatten-n 1)
+     (append)
+     ))
 
 (defun aoc/9/compress-map (block-map)
   (let ((index 0)
         (end-index (1- (length block-map))))
     (while (< index end-index)
-      (when (eq (aref block-map index) ?.)
+      (when (null (aref block-map index))
         (aset block-map index (aref block-map end-index))
         (setq end-index (1- end-index))
-        (while (eq (aref block-map end-index) ?.)
+        (while (null (aref block-map end-index))
           (setq end-index (1- end-index))))
       (setq index (1+ index)))
     (s-left (1+ end-index) block-map)))
 
 (defun aoc/9/solve ()
-  (let* ((compressed (->> (f-read-text aoc/9/input-file)
-                        (s-trim)
-                        (aoc/9/to-block-map)
-                        (s-join "")
-                        (aoc/9/compress-map))))
-    (->> compressed
-       ;; (s-split "")
-       ;; (-remove #'s-blank?)
-       ;; (-map #'string-to-number)
-       ;; (-zip-with #'* (-iota (length compressed)))
-       ;; (-sum)
-       )))
+  (let* ((block-map-list (->> (f-read-text aoc/9/input-file)
+                            (s-trim)
+                            (aoc/9/to-block-map)
+                            ))
+         (block-map-vec (cl-coerce block-map-list 'vector))
+         (compressed (aoc/9/compress-map block-map-vec))
+         (compressed-list (cl-coerce block-map-vec 'list))
+         )
+    (->> compressed-list
+       (-zip-with #'* (-iota (length compressed)))
+       (-sum)
+       )
+    ))
 
 ;; 9a
-;; TODO 92092395920 is too low
 (aoc/9/solve)
