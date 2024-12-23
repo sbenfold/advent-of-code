efc240991e2c1440b7929e821138921430f257d4
Simon Benfold <simon@benfold.com>
2024-12-23 00:31:35 +0000

    9a - incorrect solution

--- 2024/9/solution.el
+++ 2024/9/solution.el
@@ -6,8 +6,7 @@
 (defun aoc/9/expand-disk-pair (index pair)
   (concat
    (s-repeat (car pair) (number-to-string index))
-   (s-repeat (cadr pair) ".")
-   ))
+   (s-repeat (cadr pair) ".")))
 
 (defun aoc/9/to-block-map (disk-map)
   (->> (concat disk-map "0")
@@ -15,15 +14,34 @@
      (-remove #'s-blank?)
      (-map #'string-to-number)
      (-partition 2)
-     (-map-indexed #'aoc/9/expand-disk-pair)
-     )
-  )
+     (-map-indexed #'aoc/9/expand-disk-pair)))
+
+(defun aoc/9/compress-map (block-map)
+  (let ((index 0)
+        (end-index (1- (length block-map))))
+    (while (< index end-index)
+      (when (eq (aref block-map index) ?.)
+        (aset block-map index (aref block-map end-index))
+        (setq end-index (1- end-index))
+        (while (eq (aref block-map end-index) ?.)
+          (setq end-index (1- end-index))))
+      (setq index (1+ index)))
+    (s-left (1+ end-index) block-map)))
 
 (defun aoc/9/solve ()
-  (->> (f-read-text aoc/9/input-file)
-     (s-trim)
-     (aoc/9/to-block-map)
-     (s-join "")
-     ))
+  (let* ((compressed (->> (f-read-text aoc/9/input-file)
+                        (s-trim)
+                        (aoc/9/to-block-map)
+                        (s-join "")
+                        (aoc/9/compress-map))))
+    (->> compressed
+       ;; (s-split "")
+       ;; (-remove #'s-blank?)
+       ;; (-map #'string-to-number)
+       ;; (-zip-with #'* (-iota (length compressed)))
+       ;; (-sum)
+       )))
 
+;; 9a
+;; TODO 92092395920 is too low
 (aoc/9/solve)
