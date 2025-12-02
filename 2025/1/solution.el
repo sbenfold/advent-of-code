791b7c1c3c723227d2be341ae8ed0795280d7081
Simon Benfold <simbenfold@gmail.com>
2025-12-02 01:00:18 +0000

    Clean up 1a and 1b

--- 2025/1/solution.el
+++ 2025/1/solution.el
@@ -5,20 +5,15 @@
 (defun aoc/2025/1/read-file (filename)
   (->> filename (f-read) (s-trim) (s-lines)))
 
-(defun aoc/2025/1 (input)
-  (let* ((deltas (--map (->> it
-                             (s-replace-all '(("L" . "-")
-                                              ("R" . "+")))
-                             (string-to-number))
-                        input))
-         (result (->> deltas
-                      (-reductions-from #'+ 50)
-                      (-map (lambda (x) (% x 100)))
-                      (-count 'zerop)
-                      )))
-    result))
-
-(aoc/2025/1 (aoc/2025/1/read-file "input.txt"))
+(defun aoc/2025/1a (input)
+  (->> input
+       (--map (->> it
+                   (s-replace-all '(("L" . "-")
+                                    ("R" . "+")))
+                   (string-to-number)))
+       (-reductions-from #'+ 50)
+       (-map (lambda (x) (% x 100)))
+       (-count 'zerop)))
 
 (defun aoc/2025/1b (input)
   (let* ((deltas (--map (->> it
@@ -26,20 +21,17 @@
                                               ("R" . "+")))
                              (string-to-number))
                         input))
-         (red (->> deltas
-                   (-reductions-from #'+ 50)))
-         (min (-min red))
-         (amt-to-add (--> min (/ it 100) abs 1+ (* it 100)))
-         (positive (--map (+ it amt-to-add) red))
-         (pos-zipped (-zip-pair positive (cdr positive)))
-         (cycles (-map (lambda (pair)
-                         (let* ((start (car pair))
-                                (end (cdr pair))
-                                (diff (- end start))
-                                (sign (if (< diff 0) -1 0)))
-                           (- (/ (+ end sign) 100) (/ (+ start sign) 100))
-                           )) pos-zipped))
-         (result (->> cycles (-map 'abs) (-sum))))
-    result))
+         (points (->> deltas (-reductions-from #'+ 50)))
+         (amt-to-add (--> (-min points) (/ it 100) abs 1+ (* it 100)))
+         (pos-points (--map (+ it amt-to-add) points))
+         (pairs (-zip-pair pos-points (cdr pos-points)))
+         (intersections (-map (lambda (pair)
+                                (-let* (((start . end) pair)
+                                        (offset (if (< end start) -1 0)))
+                                  (- (/ (+ end offset) 100)
+                                     (/ (+ start offset) 100))
+                                  )) pairs)))
+    (->> intersections (-map 'abs) (-sum))))
 
+(aoc/2025/1a (aoc/2025/1/read-file "input.txt"))
 (aoc/2025/1b (aoc/2025/1/read-file "input.txt"))
