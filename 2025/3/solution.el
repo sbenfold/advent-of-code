c430925aa17ae2250e3d1dab34253dcc12cda968
Simon Benfold <simbenfold@gmail.com>
2025-12-03 19:23:00 +0000

    3a: Generalize to make room for 3b

--- 2025/3/solution.el
+++ 2025/3/solution.el
@@ -2,22 +2,23 @@
 (require 'f)
 (require 's)
 
-(defun aoc/2025/3/max-by-first-digit (line first-digit)
-  (let ((first-index (s-index-of first-digit line)))
-    (when (and first-index (< first-index (1- (length line))))
-      (string (aref line first-index) (seq-max (s-chop-left (1+ first-index) line))))))
-
-(defun aoc/2025/3/max-joltage (line)
-  (->> (number-sequence 9 1 -1)
-       (-map (lambda (first-digit)
-               (aoc/2025/3/max-by-first-digit line (number-to-string first-digit))
-               ))
-       (-first 'stringp)))
+(defun aoc/2025/3/max-joltage (n line)
+  (let* ((search-str (s-chop-right (1- n) line))
+         (best-char (string (seq-max search-str)))
+         (best-index (s-index-of best-char search-str))
+         )
+    (if (eq n 1)
+        best-char
+      (concat best-char
+              (aoc/2025/3/max-joltage (1- n)
+                                      (s-chop-left (1+ best-index) line))))))
 
 (defun aoc/2025/3a (filename)
   (->> filename (f-read) (s-trim) (s-lines)
-       (-map 'aoc/2025/3/max-joltage)
+       (--map (aoc/2025/3/max-joltage 2 it))
        (-map 'string-to-number)
-       (-sum)))
+       (-sum)
+       ))
 
+;; (aoc/2025/3a "example.txt")
 ;; (aoc/2025/3a "input.txt")
