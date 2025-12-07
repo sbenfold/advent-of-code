5c2fa2c01a809f3cb64b4a26d7a01f5f3e73dd70
Simon Benfold <simbenfold@gmail.com>
2025-12-07 23:26:56 +0000

    4a: Solve

--- 2025/4/solution.el
+++ 2025/4/solution.el
@@ -2,52 +2,29 @@
 (require 'f)
 (require 's)
 
-(defun aoc/2025/4/read-file (filename)
-  (->> filename (f-read) (s-trim) (s-lines) (s-join "")))
-
-;; TODO No need to unpad
-(defun aoc/2025/4/unpad (padded dim)
-  ;; (->> padded
-  ;;      (s-chop-left (+ dim 2))
-  ;;      (s-chop-right (+ dim 2))
-  ;;      ;; TODO Remove "." from start and end of each line
-  ;;      )
-  (s-replace "+" "" padded)
-  )
-(aoc/2025/4/unpad "++++++..@++@@@++@@@++++++" 3)
-
 (defun aoc/2025/4/get-adjacents (data pos dim)
   (let* ((offsets (-flatten (-table 'cons (-iota 3 -1) (-iota 3 -1))))
          (pos-offsets (--map (+ (* (car it) dim) (cdr it) pos) offsets))
          )
-    ;; TODO This is the expanded dim
     (->> pos-offsets
-         (--map (eq (aref data it) ?.))
+         (--map (eq (aref data it) ?@))
          (-non-nil)
-         (length)
-         )
-    ))
-(aoc/2025/4/get-adjacents "........@..@@@..@@@......" 12 5)
+         (length))))
 
-(defun aoc/2025/4/read-file-expanded (filename)
+(defun aoc/2025/4/a (filename)
   (let* ((lines (->> filename (f-read) (s-trim) (s-lines)))
          (dim (length (car lines)))
          (pad-line (s-repeat (+ dim 2) "."))
          (data (->> lines
                     (--map (s-wrap it "." "."))
-                    (s-join "")
-                    ))
-         (data-padded (concat pad-line data pad-line))
-         )
+                    (s-join "")))
+         (data-padded (concat pad-line data pad-line)))
     (->>
      (s-matched-positions-all "@" data-padded)
-     ;; (-unzip-lists)
      (-map 'car)
-     )
-    ;; data-padded
-    ))
-(aoc/2025/4/read-file-expanded "example2.txt")
-
-(length (aoc/2025/4/read-file "input.txt"))
-
-;; TODO Filter by < 4
+     (--map (aoc/2025/4/get-adjacents data-padded it (+ dim 2)))
+     (--filter (< it 5))
+     (length))))
+(aoc/2025/4/a "example2.txt")
+(aoc/2025/4/a "example.txt")
+(aoc/2025/4/a "input.txt")
