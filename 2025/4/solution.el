888967ac68db07e2ee307bfece062cb8710f5224
Simon Benfold <simbenfold@gmail.com>
2025-12-08 00:58:56 +0000

    4: More cleanup

--- 2025/4/solution.el
+++ 2025/4/solution.el
@@ -5,42 +5,40 @@
 (defun aoc/2025/4/read-board (filename)
   (let* ((lines (->> filename (f-read) (s-trim) (s-lines)))
          (dim (length (car lines)))
-         (data (->> lines
-                    (--map (s-wrap it "."))
-                    (s-join "")))
-         (data-padded (s-wrap data
-                              (s-repeat (+ dim 2) ".")))
-         (initial-num (length (s-matched-positions-all "@" data))))
-    (list data-padded dim initial-num)))
+         (data (-as-> lines data
+                      (--map (s-wrap it ".") data)
+                      (s-join "" data)
+                      (s-wrap data (s-repeat (+ dim 2) "."))))
+         (initial-num (s-count-matches "@" data)))
+    (list data dim initial-num)))
 
 (defun aoc/2025/4/get-adjacents (data pos dim)
   (->> (-table-flat '+ (-iota 3 (- dim) dim) (-iota 3 (- pos 1)))
        (--map (eq (aref data it) ?@))
        (-count 'identity)))
 
-(defun aoc/2025/4/get-removable (data-padded dim)
-  (->> (s-matched-positions-all "@" data-padded)
+(defun aoc/2025/4/get-removable (data dim)
+  (->> (s-matched-positions-all "@" data)
        (-map 'car)
-       (--annotate (aoc/2025/4/get-adjacents data-padded it (+ dim 2)))
+       (--annotate (aoc/2025/4/get-adjacents data it (+ dim 2)))
        (--filter (< (car it) 5))
        (-map 'cdr)))
 
 (defun aoc/2025/4/a (filename)
-  (-let* (((data-padded dim initial-num) (aoc/2025/4/read-board filename)))
-    (length (aoc/2025/4/get-removable data-padded dim))))
+  (-let* (((data dim initial-num) (aoc/2025/4/read-board filename)))
+    (length (aoc/2025/4/get-removable data dim))))
 
-(defun aoc/2025/4/b-remove-iteration (data-padded dim)
-  (let ((removable (aoc/2025/4/get-removable data-padded dim)))
+(defun aoc/2025/4/b-remove-iteration (data dim)
+  (let ((removable (aoc/2025/4/get-removable data dim)))
     (if (null removable)
-        data-padded
-      (--each removable (aset data-padded it ?.))
-      (aoc/2025/4/b-remove-iteration data-padded dim))))
+        data
+      (--each removable (aset data it ?.))
+      (aoc/2025/4/b-remove-iteration data dim))))
 
 (defun aoc/2025/4/b (filename)
-  (-let* (((data-padded dim initial-num) (aoc/2025/4/read-board filename))
-          (final-num (->> (aoc/2025/4/b-remove-iteration data-padded dim)
-                          (s-matched-positions-all "@")
-                          (length))))
+  (-let* (((data dim initial-num) (aoc/2025/4/read-board filename))
+          (final-num (->> (aoc/2025/4/b-remove-iteration data dim)
+                          (s-count-matches "@"))))
     (- initial-num final-num)))
 
 (aoc/2025/4/a "input.txt")
